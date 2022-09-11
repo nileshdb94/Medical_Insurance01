4c07561e1a0247d43e5fd0d4fdd5c8b05f864728
nileshdb <nileshdb94@gmail.com>
2022-09-11 22:42:05 +0530

    .

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+CSV_FILE_PATH = 'enter_path'
+
+PORT_NUMBER = 5008
+
+MODEL_FILE_PATH = r"D:\ND Py\Project\project_app\KNN_Regression_Model.pkl"
+
+JSON_FILE_PATH = r"D:\ND Py\Project\project_app\project_data.json"
+
+SCALING_FILE_PATH = r'D:\ND Py\Project\project_app\Std_Scaling.pkl'
